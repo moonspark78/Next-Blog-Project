da2c81778a21650f8f2b992e7c830cc1052cb013
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-15 10:27:32 +0100

    :rocket: :tada:   :rocket: :tada:

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+/* Il sert a lorsque qu'on click sur un lien, qu'on evite d'avoir une page blanche */
+
+const loading = () => {
+  return <div>loading</div>;
+};
+
+export default loading;
