285f72b53008c8c0b356225bb34d10fb244dbe24
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-22 21:17:13 +0100

    global css navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,25 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/navbar/Navbar';
-import Footer from '@/components/footer/Footer';
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'Next.js starter app',
-}
+  title: "Blog App",
+  description: "Next.js starter app",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Navbar/>
-        {children}
-      <Footer/>
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
-  )
+  );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,16 @@
-import Link from 'next/link';
-import React from 'react'
-import Links from './links/Links';
+import React from "react";
+import Links from "./links/Links";
+import styles from "./navbar.module.css"
 
 const Navbar = () => {
   return (
-    <div>
-        <div>Logo</div>
-        <div>
-            <Links/>
-        </div>
+    <div className={styles.container}>
+      <div>Logo</div>
+      <div>
+        <Links />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
